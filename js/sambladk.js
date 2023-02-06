fc741258b36ffb893e0088691ba39e2d3427103f
kristian andersen <krilleandersen@gmail.com>
2023-02-06 12:04:02 +0100

    added burger menu

--- js/sambladk.js
+++ js/sambladk.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 
     //open close FAQ elements
-    let articles = document.querySelectorAll('article');
-
+    let articles = document.querySelectorAll('.ossbox');
+   
+    let burger = document.querySelector('.hamburger-lines')
+    let mobilnav = document.querySelector('.mobilnav');
 
     for (var i=0; i < articles.length; i++) {
         articles[i].addEventListener("click", toggle);
     };
+
+
+
+        burger.addEventListener("click", toggleMobile);
+        function toggleMobile(e) {
+            mobilnav.classList.toggle("mobilnavOpen");
+        }
 
     function toggle(e) {
         e.currentTarget.classList.toggle("ossboxopen");
